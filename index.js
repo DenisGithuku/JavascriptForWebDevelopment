7e73a948e65cd03285a914b14dcc4f0dd7071ae0
githukudenis <githukudenis@gmail.com>
2023-06-20 19:55:28 +0300

    firestore crud operations

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
-const firestore = firebase.firestore()
+const firestore_db = firebase.firestore()
 
-firestore
-.collection("cities")
+// reading data
+firestore_db
+.collection('cities')
+.get()
+.then((citiesSnapshot) => {
+  citiesSnapshot.docs.forEach(doc => {
+    console.log(doc.data());
+  })
+})
+.catch(err => {
+  console.log(err.message)
+})
+
+// adding
+firestore_db
+.collection('cities')
+.add({
+  name: "Kismayo",
+  population: 6000000
+})
+.then((citiesSnapshot) => {
+  console.log("New city added successfully");
+})
+.catch(err => {
+  console.log(err.message);
+})
+
+// update
+firestore_db
+.collection('cities')
+.doc("Hqthyp2Cxfa9OvZqxz8H")
+.set({
+  population: 10000000
+}, {merge: true})
+.then(() => {
+  console.log("Document update successful");
+})
+.catch(err => {
+  console.log(err.message);
+})
+
+//delete
+firestore_db.collection('cities')
+.doc("3Wmc5MNDFNUnfgdiaZXK")
+.delete()
+.then(() => {
+  console.log('Document deleted successfully');
+})
+.catch(err => {
+  console.log(err.message);
+})
+
+// querying
+firestore_db.collection('cities')
+.where("population", "==", 10000000)
 .get()
 .then((collectionSnapshot) => {
-  collectionSnapshot.forEach(doc => {
-    document.write(doc.data().name + " \n")
-  })
+  console.log(collectionSnapshot.docs);
 })
 .catch(err => {
   console.log(err.message);
